cd02a6bbe9f902d277c2ec79d5608b2a067e44fc
IntelUser <viperbait@gmail.com>
2015-03-14 17:52:56 +0100

    Added ElasticController

--- ServersVSHackers-V1/Database/ElasticController.cs
+++ ServersVSHackers-V1/Database/ElasticController.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ServersVSHackers_V1.Database
 {
     class ElasticController : IDatabaseController
     {
+        
+        public ElasticController()
+        {
+            var node = new Uri("http://localhost:9200");
+
+            var settings = new ConnectionSettings(
+                node,
+                defaultIndex: "my-application"
+            );
+
+            var client = new ElasticClient(settings);
+        }
+
         public long Insert<T>(T item)
         {
-            throw new NotImplementedException();
+            
         }
 
         public bool RemoveDatabase(string databaseName)
