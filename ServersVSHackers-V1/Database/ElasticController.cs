6fbfa8502d4dd2be44f38d745062ae06c0111434
IntelUser <viperbait@gmail.com>
2015-03-14 17:42:40 +0100

    Added Interface IDatabaseController so that you can use more than one database in this application

---
+++ ServersVSHackers-V1/Database/ElasticController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServersVSHackers_V1.Database
+{
+    class ElasticController : IDatabaseController
+    {
+        public long Insert<T>(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveDatabase(string databaseName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Search<T>(string searchPhrase)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
