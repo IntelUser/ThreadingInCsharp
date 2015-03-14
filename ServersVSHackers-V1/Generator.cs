c41e21cb1184aae35217d4e6f1a0351cb176e977
IntelUser <viperbait@gmail.com>
2015-03-14 17:15:18 +0100

    Created Generator class which has a static method to generate random integers

---
+++ ServersVSHackers-V1/Generator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServersVSHackers_V1
+{
+    static class Generator
+    {
+        private static readonly Random Random = new Random();
+        private static readonly object SyncLock = new object();
+
+        public static int GetRandomNumber(int min, int max)
+        {
+            lock (SyncLock)
+            {
+                return Random.Next(min, max);
+            }
+        }
+    }
+}
