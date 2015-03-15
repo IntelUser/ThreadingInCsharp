d59c36a6ae337aea33eb1c190c87ae00db39e0ea
Reinier Weerts <reinier.weerts@student.stenden.com>
2015-03-15 21:47:30 +0100

    dd

---
+++ ServersVSHackers-V1/Environment.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Windows.Shapes;
+
+namespace ServersVSHackers_V1
+{
+    public static class Environment
+    {
+         public static List<Polygon> World = new List<Polygon>();
+        //boelcrap
+    }
+}
