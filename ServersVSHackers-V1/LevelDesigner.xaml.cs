f7a854f2a54a99d9bb97ec61fc110b9c9e69be1c
Reinier Weerts <reinier.weerts@student.stenden.com>
2015-03-15 13:20:53 +0100

    cleanup and leveldesigner "done"

--- ServersVSHackers-V1/LevelDesigner.xaml.cs
+++ ServersVSHackers-V1/LevelDesigner.xaml.cs
@@ -1,84 +1,113 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using TestWW3;
 
 namespace ServersVSHackers_V1
 {
     /// <summary>
-    /// Window that allows new polygons to be created.
+    ///     Window that allows new polygons to be created.
     /// </summary>
     public partial class LevelDesigner : Window
     {
-        PointCollection pointCollection = new PointCollection();
-        List<Polygon> countriesList = new List<Polygon>();
         private bool _savingCoordinates = false;
+        private int MAX_COUNTRIES = 5;
+        private int numberOfCountries;
+        private PointCollection pointCollection = new PointCollection();
+        private readonly List<Polygon> countriesList = new List<Polygon>();
+
         public LevelDesigner()
         {
             InitializeComponent();
-
         }
 
-        private void NewCountryButton_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
         private void CountryTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            if (char.IsLetter((char)e.Key)) e.Handled = true;
+            if (char.IsLetter((char) e.Key)) e.Handled = true;
         }
+
         private Polygon CreatePolygon(string Name, PointCollection pc)
         {
-            Polygon polygon = new Polygon();
+            var polygon = new Polygon();
             polygon.Name = Name;
             polygon.Points = pc;
             polygon.Stroke = new SolidColorBrush(Colors.Green);
             polygon.Fill = new SolidColorBrush(Colors.Yellow);
             var xMin = pc.Min(p => p.X);
             var yMin = pc.Min(p => p.Y);
+            MessageBox.Show(String.Format("x: {0}, y= {1}", xMin, yMin));
 
-
-            TextBlock countrylabel = new TextBlock();
+            var countrylabel = new TextBlock();
             countrylabel.Text = Name;
+            Panel.SetZIndex(countrylabel, 10);
             Canvas.SetLeft(countrylabel, xMin);
             Canvas.SetTop(countrylabel, yMin);
             CreateCanvas.Children.Add(countrylabel);
-            return polygon;            
+            return polygon;
         }
-
 
         private void SaveCountryButton_Click(object sender, RoutedEventArgs e)
         {
-
+            numberOfCountries++;
             //CreateCanvas.Children.Add(CreatePolygon(CountryTextBox.Text, pointCollection));
-            Gridx.Children.Add(CreatePolygon(CountryTextBox.Text, pointCollection));
+            var country = CreatePolygon(CountryTextBox.Text, pointCollection);
+            CreateCanvas.Children.Add(country);
+            countriesList.Add(country);
             pointCollection = null;
             pointCollection = new PointCollection();
-
+            if (numberOfCountries.Equals(5))
+            {
+                MessageBox.Show("Limit reached!");
+                SaveCountryButton.IsEnabled = false;
+                CountryTextBox.IsEnabled = false;
+            }
         }
 
-        
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {            
+        {
             {
                 //reads x-y coordinates from mouseclick on canvas
-                Point p = Mouse.GetPosition(CreateCanvas);
+                var p = Mouse.GetPosition(CreateCanvas);
                 pointCollection.Add(p);
             }
         }
 
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            CreateCanvas.Children.Clear();
+            countriesList.Clear();
+            SaveCountryButton.IsEnabled = true;
+            CountryTextBox.IsEnabled = true;
+            numberOfCountries = 0;
+        }
 
+        private void DeleteLastButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var removeCountry = countriesList.Last();
+                countriesList.Remove(removeCountry);
+                CreateCanvas.Children.Remove(removeCountry);
 
+                var removeLabel = from r in CreateCanvas.Children.OfType<TextBlock>()
+                    where r.Text == removeCountry.Name
+                    select r;
+                var result = removeLabel.First();
+                CreateCanvas.Children.Remove(result);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void SaveWorldButton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.world = countriesList;
+            this.Close();
+        }
     }
 }
--- ServersVSHackers-V1/SimulationEngine.cs
+++ ServersVSHackers-V1/SimulationEngine.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Shapes;
 using Nest;
 using ServersVSHackers_V1.Database;
 
@@ -11,6 +12,10 @@ namespace ServersVSHackers_V1
 {
     class SimulationEngine
     {
+        
+        ConcurrentBag<Hacker> ActiveHackers, BustedHackers = new ConcurrentBag<Hacker>();
+        ConcurrentBag<Server> ActiveServers, HackedServers = new ConcurrentBag<Server>();
+ 
 
         private readonly IDatabaseController _dbController;
         //private Boolean is
@@ -18,10 +23,46 @@ namespace ServersVSHackers_V1
 
         public SimulationEngine()
         {
+
             // connect to local elastic database
             var node = new Uri("http://localhost:9200");
             var settings = new ConnectionSettings(node, defaultIndex: ATTACK_LOG_TABLE);
             _dbController = new ElasticController(settings);
+        }
+
+        public void GenerateEntities()
+        {
+            ConcurrentBag<IEntity> cbEntities = new ConcurrentBag<IEntity>();
+            //one MILLION entities
+            int _numberOfEntities = 5000;
+            // balance = 1 (limit) is 10% hackers, 90% servers
+            // balance = 9 (limit) is 90% hackers, 10% servers
+            //value of Slider
+            int _balance = 6;
+
+
+            //hacker code
+            int _numberOfHackers = (_numberOfEntities * _balance) / 10;
+            int _numberOfServers = (_numberOfEntities * (10 - _balance)) / 10;
+            // balance = 1 (limit) is 10% hackers, 90% servers
+            // balance = 9 (limit) is 90% hackers, 10% servers
+
+            //generation code
+            
+
+            Parallel.For(0, _numberOfServers, i => cbEntities.Add(new Server(10, 10, new Country())));
+
+
+            Parallel.For(0, _numberOfHackers, i => cbEntities.Add(new Hacker(10, 10)));
+
+
+        }
+
+        
+        public struct ValidPoint
+        {
+            public double x;
+            public double y;
         }
 
         private void InitDatabase()
